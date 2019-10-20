79e7b023a2e32631e09e1d88673e5dfd58666e47
rizanw <rzkandre@gmail.com>
2019-10-20 23:04:08 +0700

    merging with new-theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,17 @@ if (typeof(Storage) !== "undefined") {
     console.log('Sorry! No Web Storage support..');
 }
 
-// save user state on local storage
-function switchTheme(e) {
-    if (e.target.checked) {
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
-        localStorage.setItem('theme', 'light');
-    }    
-}
-
 // run program when document ready
 document.addEventListener("DOMContentLoaded", function(event) { 
-    // check whenever the toggle is light or dark
-    const toggle = document.querySelector('input[type="checkbox"]#toggle');
-    toggle.addEventListener('change', switchTheme, false);
+    // save user state on local storage
+    function switchTheme(e) {
+        if (e.target.checked) {
+            localStorage.setItem('theme', 'dark');
+        }
+        else {
+            localStorage.setItem('theme', 'light');
+        }    
+    }
 
     // check and implement user state
     const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
@@ -29,4 +25,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             document.querySelector('input[type="checkbox"]#toggle').checked = true;
         }
     }
+
+    // check whenever the toggle is light or dark
+    const toggle = document.querySelector('input[type="checkbox"]#toggle');
+    toggle.addEventListener('change', switchTheme, false);
 });
